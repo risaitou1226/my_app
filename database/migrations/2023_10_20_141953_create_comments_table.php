fc8176770f8810e94c30813327d6b1357738f4af
risaitou1226 <pecchanpecchan@gmail.com>
2023-10-24 12:40:15 +0900

    コメント登録できない

--- database/migrations/2023_10_20_141953_create_comments_table.php
+++ database/migrations/2023_10_20_141953_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
             $table->foreignId('user_id')
                 ->constrained()
                 ->cascadeOnUpdate()
